95548f741ceac7d0f4a4862809288d0108dcea68
Iván Muerte <ivan.muerte@panel.es>
2026-04-04 07:19:45 +0200

    feat: expand SoundPoolPlayer interface with pause/pauseAll

--- core/audio/src/main/java/org/role/samples_button/core/audio/SoundPoolPlayer.kt
+++ core/audio/src/main/java/org/role/samples_button/core/audio/SoundPoolPlayer.kt
@@ -1,6 +1,8 @@
 package org.role.samples_button.core.audio
 
 interface SoundPoolPlayer {
-    fun play(filePath: String)
+    fun play(filePath: String)   // new-play or resume, manager decides
+    fun pause(filePath: String)
+    fun pauseAll()
     fun release()
 }
--- feature/soundboard/impl/src/test/java/org/role/samples_button/feature/soundboard/impl/FakeSoundPoolPlayer.kt
+++ feature/soundboard/impl/src/test/java/org/role/samples_button/feature/soundboard/impl/FakeSoundPoolPlayer.kt
@@ -4,10 +4,20 @@ import org.role.samples_button.core.audio.SoundPoolPlayer
 
 class FakeSoundPoolPlayer : SoundPoolPlayer {
     val playedPaths = mutableListOf<String>()
+    val pausedPaths = mutableListOf<String>()
+    var pauseAllCalled = false
     var released = false
 
     override fun play(filePath: String) {
         playedPaths += filePath
+    }
+
+    override fun pause(filePath: String) {
+        pausedPaths += filePath
+    }
+
+    override fun pauseAll() {
+        pauseAllCalled = true
     }
 
     override fun release() {
